e451a075a390e86a28f9934dda4ff3ce462f317d
Quentin Vansteene <vansteene.quentin@gmail.com>
2020-03-03 19:47:26 +0100

    add js

--- tabs.js
+++ tabs.js
@@ -1,11 +1,30 @@
 var Tabs = Object.getPrototypeOf(app).Tabs = new Component("tabs");
-Tabs.debug = true;
+// Tabs.debug = true;
 Tabs.createdAt      = "2.0.0";
 Tabs.lastUpdate     = "2.0.0";
 Tabs.version        = "1";
 // Tabs.factoryExclude = true;
 // Tabs.loadingMsg     = "This message will display in the console when component will be loaded.";
 
-// Tabs.prototype.onCreate = function(){
-  // do thing after element's creation
-// }
+Tabs.prototype.onCreate = function(){
+	var tabs = this;
+	tabs.nav = {$el : tabs.$el.children('.tabs__nav'),};
+	tabs.content = {$el : tabs.$el.children('.tabs__content'),};
+
+	tabs.nav.buttons = tabs.nav.$el.children('button');
+	tabs.content.tabs = tabs.content.$el.children('.tab');
+
+	tabs.nav.buttons.each(function(index,button){
+		button = $(button);
+		button.bind('click',function(event){
+			tabs.nav.buttons.removeClass('active');
+			tabs.content.tabs.removeClass('active');
+			button.addClass('active');
+			$(tabs.content.tabs[index]).addClass('active');
+		});
+	});
+	if(!tabs.nav.buttons.hasClass('active'))
+		tabs.nav.buttons.first().trigger('click');
+	else
+		tabs.nav.buttons.filter('.active').trigger('click');
+}
