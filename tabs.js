c0cabd2a75f29ff3f2b02e72034f5210bc399c4e
Quentin Vansteene <vansteene.quentin@gmail.com>
2020-03-03 18:01:07 +0100

    Initial commit

---
+++ tabs.js
@@ -0,0 +1,11 @@
+var Tabs = Object.getPrototypeOf(app).Tabs = new Component("tabs");
+Tabs.debug = true;
+Tabs.createdAt      = "2.0.0";
+Tabs.lastUpdate     = "2.0.0";
+Tabs.version        = "1";
+// Tabs.factoryExclude = true;
+// Tabs.loadingMsg     = "This message will display in the console when component will be loaded.";
+
+// Tabs.prototype.onCreate = function(){
+  // do thing after element's creation
+// }
