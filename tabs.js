0051884e847768dca3c7f5710b22e47404044b86
Quentin Vansteene <vansteene.quentin@gmail.com>
2024-02-15 11:32:25 +0100

    /!\ update for framway 2.4.5

--- tabs.js
+++ tabs.js
@@ -2,8 +2,8 @@ module.exports = function(app){
 	var Tabs = Object.getPrototypeOf(app).Tabs = new app.Component("tabs");
 	// Tabs.debug = true;
 	Tabs.createdAt      = "2.0.0";
-	Tabs.lastUpdate     = "2.0.0";
-	Tabs.version        = "1";
+	Tabs.lastUpdate     = "2.4.5";
+	Tabs.version        = "1.0.1";
 	// Tabs.factoryExclude = true;
 	// Tabs.loadingMsg     = "This message will display in the console when component will be loaded.";
 	Tabs.prototype.onCreate = function(){
