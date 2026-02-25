03115dcc0f18427db6656ce874aa9e4ab38025e7
SeptimusVII <vansteene.quentin@gmail.com>
2026-02-25 11:41:29 +0100

    conversion to framway v3

--- tabs.js
+++ tabs.js
@@ -1,57 +1,63 @@
-module.exports = function(app){
-	var Tabs = Object.getPrototypeOf(app).Tabs = new app.Component("tabs");
-	// Tabs.debug = true;
-	Tabs.createdAt      = "2.0.0";
-	Tabs.lastUpdate     = "2.6.0";
-	Tabs.version        = "1.0.5";
-	// Tabs.factoryExclude = true;
-	// Tabs.loadingMsg     = "This message will display in the console when component will be loaded.";
-	Tabs.prototype.onCreate = function(){
-		var tabs = this;
-		tabs.nav = {$el : tabs.$el.find('.tabs__nav').first(),};
-		tabs.content = {$el : tabs.$el.find('.tabs__content').first(),};
-		// callbacks
-        tabs.beforeChange = (tabs.beforeChange !== undefined) 	? tabs.beforeChange	: function(){tabs.log('beforeChange'); };
-        tabs.afterChange  = (tabs.afterChange !== undefined)	? tabs.afterChange 	: function(){ tabs.log('afterChange'); };
-        
-		tabs.nav.buttons = tabs.nav.$el.find('button,.nav__button').not('.exclude');
-		tabs.content.tabs = tabs.content.$el.find('.tab').filter(function(){
-			return $(this).closest('.tabs__content').get(0) == tabs.content.$el.get(0);
-		});
+module.exports = function(){
+    let Tabs = Object.getPrototypeOf(fw).Tabs = class Tabs extends fw.Component{
+        static {
+            this.debug = true;
+            this.createdAt  = "3.0.0";
+            this.lastUpdate = "3.0.0";
+            this.version = "1.0.0";
+            this.tpl = utils.htmlToNode(require('bundle-tpl:./tabs.html')).outerHTML;
+            // this.describe();
+        }
+        onCreate(){
+        	this.nav = {
+        		el: this.el.querySelector(':scope>.tabs__nav'),
+        		buttons: this.el.querySelector(':scope>.tabs__nav').querySelectorAll(':scope>button:not(.exclude),:scope>.nav__button')
+        	};
+        	this.content = {
+        		el: this.el.querySelector(':scope>.tabs__content'),
+        		tabs: this.el.querySelectorAll(':scope>.tabs__content>.tab')
+        	};
+	        this.beforeChange = (this.beforeChange !== undefined) ? this.beforeChange : function(){
+	    		this.log('beforeChange');
+	        };
+	        this.afterChange = (this.afterChange !== undefined)   ? this.afterChange  : function(){
+	        	this.log('afterChange');
+	        };
 
+        	// init active state
+        	if (!this.nav.el.querySelector('.active')) {
+        		this.content.tabs.forEach((tab)=>{
+        			tab.classList.remove('active')
+        		})
+        		this.nav.buttons[0].classList.add('active');
+        		this.content.tabs[0].classList.add('active');
+        	} else {
+        		this.content.tabs.forEach((tab)=>{
+        			tab.classList.remove('active')
+        		})
+        		this.content.tabs[this.getCurrentIndex()].classList.add('active')
+        	}
 
-		if(!tabs.nav.buttons.hasClass('active')){
-			// tabs.nav.buttons.first().trigger('click');
-			tabs.nav.buttons.first().addClass('active');
-			tabs.content.tabs.removeClass('active')
-			$(tabs.content.tabs).first().addClass('active');
-		}
-		else{
-			// tabs.nav.buttons.filter('.active').trigger('click');
-			tabs.content.tabs.removeClass('active');
-			$(tabs.content.tabs[tabs.nav.buttons.filter('.active').index()]).addClass('active');
-		}
-
-		return tabs;
-	}
-
-    $(function () {
-        $('body').on('click', '.tabs__nav button:not(.exclude), .nav__button', function(e) {
-        	let btn = this;
-        	let tabs = $(btn).closest('.tabs').tabs('get'); 
-        	Promise.resolve(tabs.beforeChange()).then(function(tab){
-        		tabs.nav.buttons.removeClass('active');
-        		tabs.content.tabs.removeClass('active');
-        		$(btn).addClass('active');
-        		$(tabs.content.tabs[$(btn).index()]).addClass('active');
-        		tabs.afterChange();
+        	// buttons events
+        	this.nav.buttons.forEach((btn)=>{
+    			btn.addEventListener('click',()=>{
+    				Promise.resolve(this.beforeChange()).then(()=>{
+	    				this.nav.buttons.forEach((b)=>{b.classList.remove('active'); })
+	    				this.content.tabs.forEach((tab)=>{tab.classList.remove('active'); })
+	    				btn.classList.add('active');
+	    				this.content.tabs[this.getCurrentIndex()].classList.add('active');
+	    				this.afterChange();
+    				})
+    				.catch(function(err){
+    					console.log('Error on tabs change (tab '+btn.innerHTML.trim()+'): ',err);
+    				})
+    			})
         	})
-        	.catch(function(err){
-        		console.log('Error on tabs change (tab '+btn.innerHTML.trim()+'): ',err);
-        	})
-        });
-    });
+        }
 
-	return Tabs;
+        getCurrentIndex(){
+        	return this.nav.buttons.indexOf(this.nav.el.querySelector('.active'));
+        }
+    }
+    return Tabs;
 }
-
