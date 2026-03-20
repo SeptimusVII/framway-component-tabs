4b5e71154dacee7fd45a9fa3f336892ca067fed7
SeptimusVII <vansteene.quentin@gmail.com>
2026-03-20 13:09:32 +0100

    add `getCurrentIndex` function. change the click nav button event to use it, so tabs are less dependent on exact markup

--- tabs.js
+++ tabs.js
@@ -2,8 +2,8 @@ module.exports = function(app){
 	var Tabs = Object.getPrototypeOf(app).Tabs = new app.Component("tabs");
 	// Tabs.debug = true;
 	Tabs.createdAt      = "2.0.0";
-	Tabs.lastUpdate     = "2.6.0";
-	Tabs.version        = "1.0.5";
+	Tabs.lastUpdate     = "2.8.0";
+	Tabs.version        = "1.1.0";
 	// Tabs.factoryExclude = true;
 	// Tabs.loadingMsg     = "This message will display in the console when component will be loaded.";
 	Tabs.prototype.onCreate = function(){
@@ -35,6 +35,10 @@ module.exports = function(app){
 		return tabs;
 	}
 
+	Tabs.prototype.getCurrentIndex = function(){
+		return this.nav.buttons.toArray().indexOf(this.nav.$el.get(0).querySelector('.active'));
+	}
+
     $(function () {
         $('body').on('click', '.tabs__nav button:not(.exclude), .nav__button', function(e) {
         	let btn = this;
@@ -43,7 +47,7 @@ module.exports = function(app){
         		tabs.nav.buttons.removeClass('active');
         		tabs.content.tabs.removeClass('active');
         		$(btn).addClass('active');
-        		$(tabs.content.tabs[$(btn).index()]).addClass('active');
+        		$(tabs.content.tabs[tabs.getCurrentIndex()]).addClass('active');
         		tabs.afterChange();
         	})
         	.catch(function(err){
